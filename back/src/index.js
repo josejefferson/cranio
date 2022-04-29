c84964c91317be8b3eb8323a6da2485ce9406b81
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-04-29 11:00:29 -0300

    chore(back): adicionado bibliotecas

--- back/src/index.js
+++ back/src/index.js
@@ -1,6 +1,10 @@
+require('dotenv/config')
+require('module-alias/register')
 const express = require('express')
+
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
 	res.json({ message: 'Hello World' })
@@ -10,3 +14,5 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
 	console.log('Servidor rodando na porta', PORT)
 })
+
+process.on('uncaughtException', console.error)
