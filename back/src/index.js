c08395a1a8f9b34509d9150c86438118ee3eb368
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-04-30 13:47:04 -0300

    fix(back): cors

--- back/src/index.js
+++ back/src/index.js
@@ -1,12 +1,14 @@
 require('dotenv/config')
 require('module-alias/register')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 require('./modules/database')
+app.use(cors())
 app.use('/ad', require('./routes/ad'))
 app.use('/challenge', require('./routes/challenge'))
 app.use('/student', require('./routes/student'))
