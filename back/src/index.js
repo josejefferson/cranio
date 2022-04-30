e5476143b69355444e70856e4950225271ec0e4b
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-04-30 12:54:21 -0300

    fix(back): adicionado rotas 404 e erro

--- back/src/index.js
+++ back/src/index.js
@@ -11,6 +11,14 @@ app.use('/ad', require('./routes/ad'))
 app.use('/challenge', require('./routes/challenge'))
 app.use('/student', require('./routes/student'))
 
+app.use((req, res) => {
+	res.status(404).send({ error: true, message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+	res.status(500).send({ error: true, message: err.message, details: err })
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
 	console.log('Servidor rodando na porta', PORT)
