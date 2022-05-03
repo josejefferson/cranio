6241d02d7ba94e17aae1948c97b2a41bc213b8f3
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-05-03 10:23:35 -0300

    fix(front): Type error

--- front/src/pages/ads.tsx
+++ front/src/pages/ads.tsx
@@ -1,6 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import type { GetStaticProps, NextPage } from 'next'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Image from 'next/image'
 import Carousel from 'react-bootstrap/Carousel'
 import axios from '../api/'
+
+type IAds = {
+  id: number;
+  title?: string;
+  description?: string;
+  image: string;
+  data: []
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await axios.get<IAds>('/ad/active')
@@ -17,8 +27,8 @@ const Home: NextPage<IAds> = (data) => {
       {data.data?.map((data: IAds, index: number) => {
         return (
           <Carousel.Item key={index}>
-            <img className="background-img" src={data.image} />
-            <img src={data.image} />
+            <Image className="background-img" src={data.image} alt={data.title} />
+            <Image src={data.image} alt={data.title} />
             <Carousel.Caption>
               <h3>{data.title}</h3>
               <p>{data.description}</p>
