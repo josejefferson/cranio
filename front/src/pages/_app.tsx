67ddb66e046565467b0df0a8fa44728cdd898b19
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-05-02 21:02:03 -0300

    feat(front): novo slide

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import { Chakra } from '../theme/'
+import '../styles/main.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
