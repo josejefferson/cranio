72b453e3af1633381a8354793fd3d21ab50b09bf
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-05-06 14:36:34 +0000

    feat(front): alternativas

---
+++ front/src/components/Body.tsx
@@ -0,0 +1,29 @@
+import styles from '../styles/Body.module.css'
+
+export default function Question() {
+	return (
+		<div className={styles.body}>
+			<div className={styles.image}>
+				<img src="https://www.metabase.com/images/posts/the-right-visualization/stacked-bar-chart.png" alt="Chart" />
+			</div>
+			<div className={styles.alternatives}>
+				<div className={styles.alternative}>
+					<div className={styles.alternativeNumber}>1</div>
+					<div className={styles.alternativeBody}>Lorem ipsum dolor sit amet.</div>
+				</div>
+				<div className={styles.alternative}>
+					<div className={styles.alternativeNumber}>2</div>
+					<div className={styles.alternativeBody}>Lorem ipsum dolor sit amet.</div>
+				</div>
+				<div className={styles.alternative}>
+					<div className={styles.alternativeNumber}>3</div>
+					<div className={styles.alternativeBody}>Lorem ipsum dolor sit amet.</div>
+				</div>
+				<div className={styles.alternative}>
+					<div className={styles.alternativeNumber}>4</div>
+					<div className={styles.alternativeBody}>Lorem ipsum dolor sit amet.</div>
+				</div>
+			</div>
+		</div>
+	)
+}
--- front/src/pages/test.tsx
+++ front/src/pages/test.tsx
@@ -1,11 +1,20 @@
 import Header from '../components/Header'
 import Question from '../components/Question'
+import Body from '../components/Body'
 
 export default function test() {
 	return (
 		<>
-			<Header />
-			<Question />
+			<div style={{
+				height: '100vh',
+				maxHeight: '100vh',
+				display: 'flex',
+				flexDirection: 'column'
+			}}>
+				<Header />
+				<Question />
+				<Body />
+			</div>
 		</>
 	)
 }
