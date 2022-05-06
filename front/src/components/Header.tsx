26cd33848e178766f1f4fb090dd45f1b3bbb5a83
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-05-06 13:39:12 +0000

    feat(front): componente Header e Question

---
+++ front/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Header.module.css'
+import Image from 'next/image'
+
+export default function Header() {
+	return (
+		<div className={styles.header}>
+			<div className={styles.image}>
+				<Image src="/img/skull.svg" alt="LOL" layout="fill" objectFit="contain" />
+			</div>
+			<div className={styles.title}>
+				O CRÂNIO
+			</div>
+		</div>
+	)
+}
---
+++ front/src/pages/test.tsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+import Question from '../components/Question'
+
+export default function test() {
+	return (
+		<>
+			<Header />
+			<Question />
+		</>
+	)
+}
