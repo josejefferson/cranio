f9864bf8c4d66fef495a7b663e2d5b3b010b58a4
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-05-27 13:30:46 -0300

    fix(front): corrigido bugs no timer

--- front/src/components/Challenge/Timer.tsx
+++ front/src/components/Challenge/Timer.tsx
@@ -10,17 +10,18 @@ export default function Timer({ time, active, timeOutCallback }: any) {
   // Timer
   const [timer, setTimer] = useState(time)
   useEffect(() => {
+    console.log('Iniciando timer...')
     if (active && timer > 0) {
-      const _timer = setTimeout(() => {
+      const _timer = setInterval(() => {
         setTimer(timer - 1)
         setTimeChange(true)
         setTimeout(() => setTimeChange(false), 200)
       }, 1000)
-      return () => clearTimeout(_timer)
+      return () => clearInterval(_timer)
     } else if (active && timer === 0) {
       timeOutCallback(null)
     }
-  })
+  }, [timer, active])
 
   const color = chroma.scale(['#ff595e', '#ffca3a', '#8ac926']).mode('hsl')(timer / time)
 
