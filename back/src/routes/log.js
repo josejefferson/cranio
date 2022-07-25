1a76bd3502b24e5776b7a9a309e48e0b56aa3c49
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-07-25 19:36:33 -0300

    fix(back): filtragem de logs

--- back/src/routes/log.js
+++ back/src/routes/log.js
@@ -14,20 +14,17 @@ router.use(Restrictions.admin)
  * Retorna todos os logs
  */
 router.get('/', asyncRoutes(async (req, res) => {
-	const logs = Logger.getLogs()
-	res.json(logs)
-}))
+	let filter = jsonParse(req.query.q, null, null)
+	if (!filter || Array.isArray(filter)) filter = {}
 
-/**
- * Pesquisa os logs
- */
-router.get('/search', asyncRoutes(async (req, res) => {
-	const filter = jsonParse(req.query.q, null, null)
-	if (!filter || Array.isArray(filter)) {
-		return res.status(400).send({ error: true, code: 400, message: 'Bad request' })
-	}
-	const logs = await Log.find(filter)
-	res.json(logs)
+	Log.find(filter).then((logs) => {
+		res.json(logs)
+	}).catch((err) => {
+		res.status(500).json({
+			error: true,
+			details: err
+		})
+	})
 }))
 
 module.exports = router
